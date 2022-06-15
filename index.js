9b9978e0cc5e5db4958431606b908b07dba7f192
emily knopf <emlbragg@Gmail.com>
2022-06-15 15:00:49 -0500

    commit tree 01741cde9e33fde9afe0be98889af553361c9d8f

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+function updateEmployeeWithKeyAndValue(obj, key, value){
+    const newObj = {...obj}
+    newObj[key] = value
+
+    return newObj
+
+}
+
+const employee = {
+    name : "Emily",
+    streetAddress: "123 happy way",
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value
+
+    return obj
+}
+
+function deleteFromEmployeeByKey(obj, key, value){
+    const deleteObject = {...obj}
+    deleteObject[key] = value
+
+    return deleteObject
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key, value){
+    obj[key] = value
+
+    return obj
+}
